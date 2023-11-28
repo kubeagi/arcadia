5394a5268b50b20c7e7715ef6e931e7ddb288e88
bjwswang <bjwswang@gmail.com>
2023-11-28 09:28:35 +0000

    fix: ignore already exist error in worker reconcile
    Signed-off-by: bjwswang <bjwswang@gmail.com>

--- controllers/worker_controller.go
+++ controllers/worker_controller.go
@@ -24,6 +24,7 @@ import (
 	"github.com/go-logr/logr"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -216,7 +217,10 @@ func (r *WorkerReconciler) reconcileWhenWorkerReady(ctx context.Context, logger 
 				return err
 			}
 			if err = r.Client.Create(ctx, embedder); err != nil {
-				return err
+				// Ignore error when already exists
+				if !k8serrors.IsAlreadyExists(err) {
+					return err
+				}
 			}
 		case arcadiaworker.Update:
 			// Skip update when found
@@ -237,7 +241,10 @@ func (r *WorkerReconciler) reconcileWhenWorkerReady(ctx context.Context, logger 
 				return err
 			}
 			if err = r.Client.Create(ctx, llm); err != nil {
-				return err
+				// Ignore error when already exists
+				if !k8serrors.IsAlreadyExists(err) {
+					return err
+				}
 			}
 		case arcadiaworker.Update:
 			// Skip update when found
