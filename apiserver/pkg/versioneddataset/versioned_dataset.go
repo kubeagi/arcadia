a1ff2e1916e2509bf15df385100c7b3cf280134e
y9rabbito <skbs74784@gmail.com>
2024-01-10 23:37:52 +0530

    Handle Incorrect Time

--- apiserver/pkg/versioneddataset/versioned_dataset.go
+++ apiserver/pkg/versioneddataset/versioned_dataset.go
@@ -133,11 +133,13 @@ func VersionFiles(ctx context.Context, c dynamic.Interface, input *generated.Ver
 	})
 
 	result := make([]generated.PageNode, 0)
+	var lastModifiedTime time.Time
 	for _, obj := range objectInfoList {
 		if keyword == "" || strings.Contains(obj.Key, keyword) {
+			lastModifiedTime = obj.LastModified
 			tf := generated.F{
 				Path: strings.TrimPrefix(obj.Key, prefix),
-				Time: &obj.LastModified,
+				Time: &lastModifiedTime,
 			}
 
 			size := utils.BytesToSizedStr(obj.Size)
