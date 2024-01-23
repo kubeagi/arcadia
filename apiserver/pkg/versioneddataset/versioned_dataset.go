f2d642a1d82482ef4fa3ade79ac573b5d6b9177f
y9rabbito <66154871+y9rabbito@users.noreply.github.com>
2024-01-23 15:03:24 +0530

    Handle Incorrect Time (#540)
    * Handle Incorrect Time
    
    * Changes made regarding incorrect time
    
    * Fix the linting error
    
    * Fix linting error

--- apiserver/pkg/versioneddataset/versioned_dataset.go
+++ apiserver/pkg/versioneddataset/versioned_dataset.go
@@ -135,9 +135,10 @@ func VersionFiles(ctx context.Context, c dynamic.Interface, input *generated.Ver
 	result := make([]generated.PageNode, 0)
 	for _, obj := range objectInfoList {
 		if keyword == "" || strings.Contains(obj.Key, keyword) {
+			lastModifiedTime := obj.LastModified
 			tf := generated.F{
 				Path: strings.TrimPrefix(obj.Key, prefix),
-				Time: &obj.LastModified,
+				Time: &lastModifiedTime,
 			}
 
 			size := utils.BytesToSizedStr(obj.Size)
