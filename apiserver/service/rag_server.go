2f779b27d838991fe2708226548de605a90119e9
0xff-dev <stevenshuang521@gmail.com>
2024-02-27 11:13:39 +0800

    refactor: change the report's scatter structure

--- apiserver/service/rag_server.go
+++ apiserver/service/rag_server.go
@@ -51,14 +51,12 @@ const (
 // @Produce		json
 // @Param			ragName		query		string	true	"rag name"
 // @Param			namespace	header		string	true	"Name of the bucket"
-// @Param			appName		query		string	true	"application name"
 // @Success		200			{object}	rag.Report
 // @Failure		400			{object}	map[string]string
 // @Failure		500			{object}	map[string]string
 // @Router			/rags/report [get]
 func (r *RagAPI) Summary(ctx *gin.Context) {
 	ragName := ctx.Query(ragNameQuery)
-	appName := ctx.Query(appNameQuery)
 	namespace := ctx.GetHeader(namespaceHeadr)
 
 	rr := v1alpha1.RAG{}
@@ -76,7 +74,7 @@ func (r *RagAPI) Summary(ctx *gin.Context) {
 		thresholds[string(param.Kind)] = float64(param.ToleranceThreshbold) / 100.0
 	}
 
-	report, err := rag.ParseSummary(ctx.Request.Context(), r.c, appName, ragName, namespace, thresholds)
+	report, err := rag.ParseSummary(ctx.Request.Context(), r.c, rr.Spec.Application.Name, ragName, namespace, thresholds)
 	if err != nil {
 		klog.Errorf("an error occurred generating the report, error %s", err)
 		ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
@@ -95,7 +93,6 @@ func (r *RagAPI) Summary(ctx *gin.Context) {
 // @Produce		json
 // @Param			ragName		query		string	true	"rag name"
 // @Param			namespace	header		string	true	"Name of the bucket"
-// @Param			appName		query		string	true	"application name"
 // @Param			page		query		int		false	"default is 1"
 // @Param			size		query		string	false	"default is 10"
 // @Param			sortBy		query		string	false	"rag metrcis"
@@ -110,10 +107,19 @@ func (r *RagAPI) ReportDetail(ctx *gin.Context) {
 	sortBy := ctx.Query("sortBy")
 	order := ctx.DefaultQuery("order", "desc")
 	ragName := ctx.Query(ragNameQuery)
-	appName := ctx.Query(appNameQuery)
 	namespace := ctx.GetHeader(namespaceHeadr)
 
-	result, err := rag.ParseResult(ctx.Request.Context(), r.c, page, pageSize, appName, ragName, namespace, sortBy, order)
+	rr := v1alpha1.RAG{}
+	if err := r.c.Get(ctx, types.NamespacedName{
+		Namespace: namespace, Name: ragName,
+	}, &rr); err != nil {
+		klog.Error(fmt.Sprintf("can't get rag by name %s", ragName))
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"message": fmt.Sprintf("can't get rag by name %s", ragName),
+		})
+		return
+	}
+	result, err := rag.ParseResult(ctx.Request.Context(), r.c, page, pageSize, rr.Spec.Application.Name, ragName, namespace, sortBy, order)
 	if err != nil {
 		ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
 			"message": err.Error(),
@@ -121,6 +127,44 @@ func (r *RagAPI) ReportDetail(ctx *gin.Context) {
 		return
 	}
 	ctx.JSON(http.StatusOK, result)
+}
+
+// @Summary	Get scatter data of a rag
+// @Schemes
+// @Description	Get scatter data of a rag
+// @Tags			RAG
+// @Accept			json
+// @Produce		json
+// @Param			ragName		query		string	true	"rag name"
+// @Param			namespace	header		string	true	"Name of the bucket"
+// @Success		200			{object}	rag.ReportDetail
+// @Failure		400			{object}	map[string]string
+// @Failure		500			{object}	map[string]string
+// @Router			/rags/scatter [get]
+func (r *RagAPI) ReportScatter(ctx *gin.Context) {
+	ragName := ctx.Query(ragNameQuery)
+	namespace := ctx.GetHeader(namespaceHeadr)
+
+	rr := v1alpha1.RAG{}
+	if err := r.c.Get(ctx, types.NamespacedName{
+		Namespace: namespace, Name: ragName,
+	}, &rr); err != nil {
+		klog.Error(fmt.Sprintf("can't get rag by name %s", ragName))
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"message": fmt.Sprintf("can't get rag by name %s", ragName),
+		})
+		return
+	}
+	result, err := rag.PraseScatterChart(ctx.Request.Context(), r.c, rr.Spec.Application.Name, ragName, namespace)
+	if err != nil {
+		ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+			"message": err.Error(),
+		})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{
+		"data": result,
+	})
 }
 
 func registerRAG(g *gin.RouterGroup, conf gqlconfig.ServerConfig) {
@@ -133,4 +177,5 @@ func registerRAG(g *gin.RouterGroup, conf gqlconfig.ServerConfig) {
 
 	g.GET("/report", auth.AuthInterceptor(conf.EnableOIDC, oidc.Verifier, v1alpha1.GroupVersion, "get", "rags"), api.Summary)
 	g.GET("/detail", auth.AuthInterceptor(conf.EnableOIDC, oidc.Verifier, v1alpha1.GroupVersion, "get", "rags"), api.ReportDetail)
+	g.GET("/scatter", auth.AuthInterceptor(conf.EnableOIDC, oidc.Verifier, v1alpha1.GroupVersion, "get", "rags"), api.ReportScatter)
 }
