d7b19565d4253cec81bd4f7a67391f7b931409da
0xff-dev <stevenshuang521@gmail.com>
2024-03-08 15:49:38 +0800

    feat: support editing csv file online

--- apiserver/pkg/common/read_csv.go
+++ apiserver/pkg/common/read_csv.go
@@ -16,14 +16,38 @@ limitations under the License.
 package common
 
 import (
+	"bytes"
 	"encoding/csv"
 	"io"
+	"sort"
 )
 
-type ReadCSVResult struct {
-	Rows  [][]string `json:"rows"`
-	Total int64      `json:"total"`
-}
+type (
+	ReadCSVResult struct {
+		Rows    [][]string `json:"rows"`
+		Total   int64      `json:"total"`
+		Version string     `json:"version"`
+	}
+
+	CSVLine struct {
+		LinNumber uint64   `json:"lineNumber,omitempty"`
+		Values    []string `json:"values,omitempty"`
+	}
+	UpdateCSVBody struct {
+		// BucketName string `json:"bucketName"`, from header
+		BucketPath string `json:"bucketPath"`
+		FileName   string `json:"fileName"`
+		Version    string `json:"version"`
+
+		// The version to be edited is inconsistent with the latest version.
+		// Do you want to force an update?
+		ForceUpdate bool `json:"forceUpdate"`
+
+		UpdateLines []CSVLine  `json:"updateLines"`
+		NewLines    [][]string `json:"newLines,omitempty"`
+		DelLines    []int      `json:"delLines"`
+	}
+)
 
 // ReadCSV Reads the contents of a csv file by lines according to startLine and lines,
 // if cacheLines=0 then the total number of lines in the file will be counted,
@@ -70,4 +94,65 @@ func ReadCSV(o io.Reader, startLine, lines, cachedTotalLines int64) (ReadCSVResu
 		result.Total = cachedTotalLines
 	}
 	return result, err
+}
+
+func EditCSV(r io.Reader, updateLines []CSVLine, addLines [][]string, delLines []int) (io.Reader, int64, error) {
+	reader := csv.NewReader(r)
+
+	sort.Slice(updateLines, func(i, j int) bool {
+		return updateLines[i].LinNumber < updateLines[j].LinNumber
+	})
+	sort.Ints(delLines)
+
+	var (
+		line   []string
+		err    error
+		ui, di int
+		write  bool
+	)
+
+	buf := bytes.NewBuffer([]byte{})
+	w := csv.NewWriter(buf)
+
+	li := 0
+	for ; ; li++ {
+		line, err = reader.Read()
+		if err != nil {
+			if err != io.EOF {
+				return nil, 0, err
+			}
+			break
+		}
+		if li == 0 {
+			// write header
+			if err = w.Write(line); err != nil {
+				return nil, 0, err
+			}
+			for _, newLine := range addLines {
+				if err = w.Write(newLine); err != nil {
+					return nil, 0, err
+				}
+			}
+			continue
+		}
+
+		write = true
+		if di < len(delLines) && li == delLines[di] {
+			write = false
+			di++
+		}
+
+		if ui < len(updateLines) && updateLines[ui].LinNumber == uint64(li) {
+			line = updateLines[ui].Values
+			ui++
+		}
+		if !write {
+			continue
+		}
+		if err = w.Write(line); err != nil {
+			return nil, 0, err
+		}
+	}
+	w.Flush()
+	return buf, int64(len(buf.Bytes())), nil
 }
--- apiserver/pkg/common/read_csv_test.go
+++ apiserver/pkg/common/read_csv_test.go
@@ -48,28 +48,28 @@ func TestReadCSV(t *testing.T) {
 	reader := bytes.NewReader([]byte(csvData))
 	for _, tc := range []input{
 		{
-			1, 1, 0, ReadCSVResult{[][]string{{"a", "a", "a"}}, 14}, io.EOF,
+			1, 1, 0, ReadCSVResult{[][]string{{"a", "a", "a"}}, 14, ""}, io.EOF,
 		},
 		{
-			1, 2, 14, ReadCSVResult{[][]string{{"a", "a", "a"}, {"b", "b", "b"}}, 14}, nil,
+			1, 2, 14, ReadCSVResult{[][]string{{"a", "a", "a"}, {"b", "b", "b"}}, 14, ""}, nil,
 		},
 		{
-			2, 1, 14, ReadCSVResult{[][]string{{"b", "b", "b"}}, 14}, nil,
+			2, 1, 14, ReadCSVResult{[][]string{{"b", "b", "b"}}, 14, ""}, nil,
 		},
 		{
-			2, 2, 0, ReadCSVResult{[][]string{{"b", "b", "b"}, {"c", "c", "c"}}, 14}, io.EOF,
+			2, 2, 0, ReadCSVResult{[][]string{{"b", "b", "b"}, {"c", "c", "c"}}, 14, ""}, io.EOF,
 		},
 		{
-			9, 10, 14, ReadCSVResult{[][]string{{"1", "1", "1"}, {"2", "2", "2"}, {"3", "3", "3"}, {"4", "4", "4"}, {"5", "5", "5"}, {"6", "6", "6"}}, 14}, io.EOF,
+			9, 10, 14, ReadCSVResult{[][]string{{"1", "1", "1"}, {"2", "2", "2"}, {"3", "3", "3"}, {"4", "4", "4"}, {"5", "5", "5"}, {"6", "6", "6"}}, 14, ""}, io.EOF,
 		},
 		{
-			14, 1, 0, ReadCSVResult{[][]string{{"6", "6", "6"}}, 14}, io.EOF,
+			14, 1, 0, ReadCSVResult{[][]string{{"6", "6", "6"}}, 14, ""}, io.EOF,
 		},
 		{
-			14, 2, 0, ReadCSVResult{[][]string{{"6", "6", "6"}}, 14}, io.EOF,
+			14, 2, 0, ReadCSVResult{[][]string{{"6", "6", "6"}}, 14, ""}, io.EOF,
 		},
 		{
-			8, 3, 14, ReadCSVResult{[][]string{{"i", "i", "i"}, {"1", "1", "1"}, {"2", "2", "2"}}, 14}, nil,
+			8, 3, 14, ReadCSVResult{[][]string{{"i", "i", "i"}, {"1", "1", "1"}, {"2", "2", "2"}}, 14, ""}, nil,
 		},
 		{
 			1, 15, 0, ReadCSVResult{[][]string{
@@ -87,7 +87,7 @@ func TestReadCSV(t *testing.T) {
 				{"4", "4", "4"},
 				{"5", "5", "5"},
 				{"6", "6", "6"},
-			}, 14}, io.EOF,
+			}, 14, ""}, io.EOF,
 		},
 		{
 			1, 14, 14, ReadCSVResult{[][]string{
@@ -105,7 +105,7 @@ func TestReadCSV(t *testing.T) {
 				{"4", "4", "4"},
 				{"5", "5", "5"},
 				{"6", "6", "6"},
-			}, 14}, io.EOF,
+			}, 14, ""}, io.EOF,
 		},
 		{
 			1, 13, 14, ReadCSVResult{[][]string{
@@ -122,11 +122,11 @@ func TestReadCSV(t *testing.T) {
 				{"3", "3", "3"},
 				{"4", "4", "4"},
 				{"5", "5", "5"},
-			}, 14}, nil,
+			}, 14, ""}, nil,
 		},
 
 		{
-			15, 2, 0, ReadCSVResult{nil, 14}, io.EOF,
+			15, 2, 0, ReadCSVResult{nil, 14, ""}, io.EOF,
 		},
 		{
 			// page=1, size=3
@@ -134,7 +134,7 @@ func TestReadCSV(t *testing.T) {
 				{"a", "a", "a"},
 				{"b", "b", "b"},
 				{"c", "c", "c"},
-			}, 14}, io.EOF,
+			}, 14, ""}, io.EOF,
 		},
 		{
 			// page=2,size=3
@@ -142,7 +142,7 @@ func TestReadCSV(t *testing.T) {
 				{"d", "d", "d"},
 				{"e", "e", "e"},
 				{"f", "f", "f"},
-			}, 14}, nil,
+			}, 14, ""}, nil,
 		},
 		{
 			// page=3,size=3
@@ -150,7 +150,7 @@ func TestReadCSV(t *testing.T) {
 				{"g", "g", "g"},
 				{"i", "i", "i"},
 				{"1", "1", "1"},
-			}, 14}, nil,
+			}, 14, ""}, nil,
 		},
 		{
 			// page=4,size=3
@@ -158,25 +158,25 @@ func TestReadCSV(t *testing.T) {
 				{"2", "2", "2"},
 				{"3", "3", "3"},
 				{"4", "4", "4"},
-			}, 14}, nil,
+			}, 14, ""}, nil,
 		},
 		{
 			// page=5,size=3
 			13, 3, 14, ReadCSVResult{[][]string{
 				{"5", "5", "5"},
 				{"6", "6", "6"},
-			}, 14}, io.EOF,
+			}, 14, ""}, io.EOF,
 		},
 		{
 			13, 3, 0, ReadCSVResult{[][]string{
 				{"5", "5", "5"},
 				{"6", "6", "6"},
-			}, 14}, io.EOF,
+			}, 14, ""}, io.EOF,
 		},
 		{
 			14, 1, 0, ReadCSVResult{[][]string{
 				{"6", "6", "6"},
-			}, 14}, io.EOF,
+			}, 14, ""}, io.EOF,
 		},
 		{
 			4, 5, 14, ReadCSVResult{[][]string{
@@ -185,12 +185,204 @@ func TestReadCSV(t *testing.T) {
 				{"f", "f", "f"},
 				{"g", "g", "g"},
 				{"i", "i", "i"},
-			}, 14}, nil,
+			}, 14, ""}, nil,
 		},
 	} {
 		if r, err := ReadCSV(reader, tc.startLine, tc.size, tc.cacheLInes); err != tc.err || !reflect.DeepEqual(tc.exp, r) {
 			t.Fatalf("with input %+v expect %+v get %+v error %s", tc, tc.exp, r, err)
 		}
 		_, _ = reader.Seek(0, io.SeekStart)
+	}
+}
+
+const (
+	editCsv = `hc1,hc2
+a,a
+b,b
+c,c
+d,d
+`
+)
+
+func TestEditCSV(t *testing.T) {
+	type input struct {
+		body     UpdateCSVBody
+		expbytes int64
+		exp      string
+	}
+	for _, tc := range []input{
+		{
+			body:     UpdateCSVBody{UpdateLines: []CSVLine{{LinNumber: 1, Values: []string{"A", "A"}}}},
+			expbytes: int64(len(editCsv)),
+			exp: `hc1,hc2
+A,A
+b,b
+c,c
+d,d
+`,
+		},
+
+		{body: UpdateCSVBody{UpdateLines: []CSVLine{{LinNumber: 1, Values: []string{"A", "A"}}, {LinNumber: 2, Values: []string{"B", "B"}}}},
+			expbytes: int64(len(editCsv)),
+			exp: `hc1,hc2
+A,A
+B,B
+c,c
+d,d
+`,
+		},
+		{body: UpdateCSVBody{UpdateLines: []CSVLine{{LinNumber: 1, Values: []string{"A", "A"}}, {LinNumber: 2, Values: []string{"B", "B"}}, {LinNumber: 3, Values: []string{"C", "C"}}}},
+			expbytes: int64(len(editCsv)),
+			exp: `hc1,hc2
+A,A
+B,B
+C,C
+d,d
+`,
+		},
+		{body: UpdateCSVBody{UpdateLines: []CSVLine{{LinNumber: 1, Values: []string{"A", "A"}}, {LinNumber: 2, Values: []string{"B", "B"}}, {LinNumber: 3, Values: []string{"C", "C"}}, {LinNumber: 4, Values: []string{"D", "D"}}}},
+			expbytes: int64(len(editCsv)),
+			exp: `hc1,hc2
+A,A
+B,B
+C,C
+D,D
+`},
+		{
+			body:     UpdateCSVBody{UpdateLines: []CSVLine{{LinNumber: 1, Values: []string{"A", "A"}}}, DelLines: []int{1, 2, 3, 4}},
+			expbytes: int64(8),
+			exp: `hc1,hc2
+`,
+		},
+		{
+			body:     UpdateCSVBody{UpdateLines: []CSVLine{{LinNumber: 1, Values: []string{"A", "A"}}}, DelLines: []int{1, 2, 3}},
+			expbytes: int64(12),
+			exp: `hc1,hc2
+d,d
+`,
+		},
+		{
+			body:     UpdateCSVBody{UpdateLines: []CSVLine{{LinNumber: 1, Values: []string{"A", "A"}}}, DelLines: []int{1, 2}},
+			expbytes: int64(16),
+			exp: `hc1,hc2
+c,c
+d,d
+`,
+		},
+		{
+			body:     UpdateCSVBody{UpdateLines: []CSVLine{{LinNumber: 1, Values: []string{"A", "A"}}}, DelLines: []int{1}},
+			expbytes: int64(20),
+			exp: `hc1,hc2
+b,b
+c,c
+d,d
+`,
+		},
+		{
+			body:     UpdateCSVBody{UpdateLines: []CSVLine{{LinNumber: 1, Values: []string{"A", "A"}}}, DelLines: []int{1}},
+			expbytes: int64(20),
+			exp: `hc1,hc2
+b,b
+c,c
+d,d
+`,
+		},
+		{
+			body:     UpdateCSVBody{NewLines: [][]string{{"line1", "value1"}, {"line2", "value2"}}, DelLines: []int{1}},
+			expbytes: int64(46),
+			exp: `hc1,hc2
+line1,value1
+line2,value2
+b,b
+c,c
+d,d
+`,
+		},
+		{
+			body:     UpdateCSVBody{NewLines: [][]string{{"line1", "value1"}, {"line2", "value2"}}, DelLines: []int{1, 2}},
+			expbytes: int64(42),
+			exp: `hc1,hc2
+line1,value1
+line2,value2
+c,c
+d,d
+`,
+		},
+		{
+			body:     UpdateCSVBody{NewLines: [][]string{{"line1", "value1"}, {"line2", "value2"}}, DelLines: []int{1, 2, 3}},
+			expbytes: int64(38),
+			exp: `hc1,hc2
+line1,value1
+line2,value2
+d,d
+`,
+		},
+		{
+			body:     UpdateCSVBody{NewLines: [][]string{{"line1", "value1"}, {"line2", "value2"}}, DelLines: []int{1, 2, 3, 4}},
+			expbytes: int64(34),
+			exp: `hc1,hc2
+line1,value1
+line2,value2
+`,
+		},
+		{
+			body:     UpdateCSVBody{NewLines: [][]string{{"line1", "value1"}, {"line2", "value2"}}, DelLines: []int{1, 2, 3, 4}},
+			expbytes: int64(34),
+			exp: `hc1,hc2
+line1,value1
+line2,value2
+`,
+		},
+		{
+			body:     UpdateCSVBody{UpdateLines: []CSVLine{{LinNumber: 4, Values: []string{"e", "e"}}}, NewLines: [][]string{{"line1", "value1"}}, DelLines: []int{1, 2, 3}},
+			expbytes: 25,
+			exp: `hc1,hc2
+line1,value1
+e,e
+`,
+		},
+		{
+			body: UpdateCSVBody{UpdateLines: []CSVLine{{LinNumber: 4, Values: []string{"e", "e"}}, {LinNumber: 3, Values: []string{"f", "f"}}},
+				NewLines: [][]string{{"line1", "value1"}}, DelLines: []int{1, 2}},
+			expbytes: 29,
+			exp: `hc1,hc2
+line1,value1
+f,f
+e,e
+`,
+		},
+		{
+			body: UpdateCSVBody{UpdateLines: []CSVLine{{LinNumber: 4, Values: []string{"e", "e"}}, {LinNumber: 2, Values: []string{"f", "f"}}, {LinNumber: 3, Values: []string{"i", "i"}}},
+				NewLines: [][]string{{"line1", "value1"}}, DelLines: []int{1}},
+			expbytes: 33,
+			exp: `hc1,hc2
+line1,value1
+f,f
+i,i
+e,e
+`,
+		},
+		{
+			body:     UpdateCSVBody{UpdateLines: []CSVLine{{LinNumber: 4, Values: []string{"e", "e"}}}, NewLines: [][]string{{"line1", "value1"}}},
+			expbytes: 37,
+			exp: `hc1,hc2
+line1,value1
+a,a
+b,b
+c,c
+e,e
+`,
+		},
+	} {
+		r := bytes.NewBufferString(editCsv)
+		result, l, _ := EditCSV(r, tc.body.UpdateLines, tc.body.NewLines, tc.body.DelLines)
+		if l != tc.expbytes {
+			t.Fatalf("expect %d bytes, actual are %d bytes", tc.expbytes, l)
+		}
+
+		data, _ := io.ReadAll(result)
+		if string(data) != tc.exp {
+			t.Fatalf("expect %v get %v", tc.exp, data)
+		}
 	}
 }
