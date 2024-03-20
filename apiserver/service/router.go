39ca07de34b02c2d9621343cc7daf7762645f923
0xff-dev <stevenshuang521@gmail.com>
2024-03-20 17:13:51 +0800

    fix: apiserver cors error

--- apiserver/service/router.go
+++ apiserver/service/router.go
@@ -31,7 +31,8 @@ import (
 func Cors() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		method := c.Request.Method
-		c.Header("Access-Control-Allow-Origin", "*")
+		origin := c.GetHeader("origin")
+		c.Header("Access-Control-Allow-Origin", origin)
 		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
 		c.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, namespace, Referer, User-Agent")
 		c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Cache-Control, Content-Language, Content-Type")
